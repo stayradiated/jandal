73876f0767f70b5cb849dfeba4ef00dcbae7bc0e
George Czabania <george@czabania.com>
2014-01-26 09:21:20 +1300

    [0.0.12] - Move client.js to require(‘jandal/client’); and allow custom handlers with Jandal.handle(object)

--- source/jandal.js
+++ source/jandal.js
@@ -17,14 +17,26 @@
    * Choose how to attach to the socket
    *
    * - name (string) : a key from handles (currently 'node' or 'sockjs')
+   * - name (object) : a handle in the form of
+   *   {
+   *     open: fn(socket, fn),
+   *     close: fn(socket, fn),
+   *     read: fn(socket, fn),
+   *     error: fn(socket, fn),
+   *     write: fn(socket, message)
+   *   }
    */
 
   Socket.handle = function (name) {
-    var handle = handles[name];
-    if (! handle) {
-      throw new Error('Jandal handler "' + name + '"could not be found');
+    if (typeof name === 'object') {
+      Socket._handle = name;
+    } else {
+      var handle = handles[name];
+      if (! handle) {
+        throw new Error('Jandal handler "' + name + '"could not be found');
+      }
+      Socket._handle = handle;
     }
-    Socket._handle = handle;
   };
 
   module.exports = Socket;
